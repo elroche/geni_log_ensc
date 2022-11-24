04b778d38194d479c9a1a15959697f72fe647ce8
Rebecca Grenet <rebeccagnt@orange.fr>
2022-11-24 09:04:26 +0100

    init repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace projet_elea_rebecca_gr3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
